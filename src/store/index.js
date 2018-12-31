12ea05f84c3f4f6f0088e61f493e7ae2eeca31b0
Angelo Cordon <angelocordon@gmail.com>
2018-12-30 21:52:31 -0800

    Remove unnecessary getter

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,6 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: ColumnsData,
-  getters: {
-    getColumn(id) {
-      return this.state.columns.find(column => column.id === id);
-    }
-  },
   mutations: {
     addTask(state, payload) {
       const task = payload.assignedTask;
